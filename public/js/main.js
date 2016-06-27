7637bd10f1246fcdfef34e6ce591b5824687bf79
bmmanson <bmmanson@gmail.com>
2016-06-27 19:25:20 -0400

    multiple players with bugs

--- public/js/main.js
+++ public/js/main.js
@@ -352,3 +352,167 @@ function reset() {
 };
 
 
+
+
+(function() {
+    var pressedKeys = {};
+
+    function setKey(event, status) {
+        var code = event.keyCode;
+        var key;
+
+        switch(code) {
+        case 32:
+            key = 'SPACE'; break;
+        case 37:
+            key = 'LEFT'; break;
+        case 38:
+            key = 'UP'; break;
+        case 39:
+            key = 'RIGHT'; break;
+        case 40:
+            key = 'DOWN'; break;
+        default:
+            // Convert ASCII codes to letters
+            key = String.fromCharCode(code);
+        }
+
+        pressedKeys[key] = status;
+    }
+
+    document.addEventListener('keydown', function(e) {
+        setKey(e, true);
+    });
+
+    document.addEventListener('keyup', function(e) {
+        setKey(e, false);
+    });
+
+    window.addEventListener('blur', function() {
+        pressedKeys = {};
+    });
+
+    window.input = {
+        isDown: function(key) {
+            return pressedKeys[key.toUpperCase()];
+        }
+    };
+})();
+
+(function() {
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
+
+    // Load an image url or an array of image urls
+    function load(urlOrArr) {
+        if(urlOrArr instanceof Array) {
+            urlOrArr.forEach(function(url) {
+                _load(url);
+            });
+        }
+        else {
+            _load(urlOrArr);
+        }
+    }
+
+    function _load(url) {
+        if(resourceCache[url]) {
+            return resourceCache[url];
+        }
+        else {
+            var img = new Image();
+            img.onload = function() {
+                resourceCache[url] = img;
+                
+                if(isReady()) {
+                    readyCallbacks.forEach(function(func) { func(); });
+                }
+            };
+            resourceCache[url] = false;
+            img.src = url;
+        }
+    }
+
+    function get(url) {
+        return resourceCache[url];
+    }
+
+    function isReady() {
+        var ready = true;
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
+               !resourceCache[k]) {
+                ready = false;
+            }
+        }
+        return ready;
+    }
+
+    function onReady(func) {
+        readyCallbacks.push(func);
+    }
+
+    window.resources = { 
+        load: load,
+        get: get,
+        onReady: onReady,
+        isReady: isReady
+    };
+})();
+
+(function() {
+    function Sprite(url, pos, size, speed, frames, dir, once) {
+        this.pos = pos;
+        this.size = size;
+        this.speed = typeof speed === 'number' ? speed : 0;
+        this.frames = frames;
+        this._index = 0;
+        this.url = url;
+        this.dir = dir || 'horizontal';
+        this.once = once;
+    };
+
+    Sprite.prototype = {
+        update: function(dt) {
+            this._index += this.speed*dt;
+        },
+
+        render: function(ctx) {
+            var frame;
+
+            if(this.speed > 0) {
+                var max = this.frames.length;
+                var idx = Math.floor(this._index);
+                frame = this.frames[idx % max];
+
+                if(this.once && idx >= max) {
+                    this.done = true;
+                    return;
+                }
+            }
+            else {
+                frame = 0;
+            }
+
+
+            var x = this.pos[0];
+            var y = this.pos[1];
+
+            if(this.dir == 'vertical') {
+                y += frame * this.size[1];
+            }
+            else {
+                x += frame * this.size[0];
+            }
+
+            ctx.drawImage(resources.get(this.url),
+                          x, y,
+                          this.size[0], this.size[1],
+                          0, 0,
+                          this.size[0], this.size[1]);
+        }
+    };
+
+    window.Sprite = Sprite;
+})();
