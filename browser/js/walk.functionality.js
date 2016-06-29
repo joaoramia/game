9360cf8ef06a8666ceb159cb0f06bdbf1ba64a37
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-06-29 11:42:52 -0400

    fixed walking

--- browser/js/walk.functionality.js
+++ browser/js/walk.functionality.js
@@ -2,16 +2,14 @@ function walk(x, y, dt){
     currentSelection.forEach(function(unit, index){
         //test case for only allowing cap guys to walk (so when we select buildings it doesnt work)
         if (unit.sprite.url === 'img/capguy-walk.png'){
-			if (unit.pos[0] < x) unit.pos[0] += 50*dt;
-			if (unit.pos[0] > x) unit.pos[0] -= 50*dt;
-			if (unit.pos[1] < y) unit.pos[1] += 50*dt;
-			if (unit.pos[1] > y) unit.pos[1] -= 50*dt;
+            if (Math.abs(unit.pos[0] - x) > 5 || Math.abs(unit.pos[1] - y) > 5){
+                console.log("X: ", x, "Y: ", y, "DT: ", dt, "UNIT POS X: ", unit.pos[0], "UNIT POS Y: ", unit.pos[1]);
+    			if (unit.pos[0] < x) unit.pos[0] += 50*dt;
+    			if (unit.pos[0] > x) unit.pos[0] -= 50*dt;
+    			if (unit.pos[1] < y) unit.pos[1] += 50*dt;
+    			if (unit.pos[1] > y) unit.pos[1] -= 50*dt;
 
-            if (Math.abs(unit.pos[0] - x) <= 5 && Math.abs(unit.pos[1] - y) <= 5) {
-                rightClick.x = undefined;
-                rightClick.y = undefined;
-                unit.sprite.selected = false;
-                currentSelection.splice(index, 1);
+                unit.sprite._index += 0.25;
             }
             unit.sprite.update();
 		}
