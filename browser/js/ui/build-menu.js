aa4e475827490b55088b37509a32366ab8c37807
bmmanson <bmmanson@gmail.com>
2016-07-01 16:17:23 -0400

    fixed a small bug

--- browser/js/ui/build-menu.js
+++ browser/js/ui/build-menu.js
@@ -1,10 +1,3 @@
 
-var buildMenuButtons = [
-	{text: "Bar (K)", tagName: "build-bar", clickFunction: placeholderFunction},
-	{text: "Bank (H)", tagName: "build-bank", clickFunction: placeholderFunction}
-]
 
-function updateForBuildMenu (){
-	changeButtonsMessage("Select a building");
-	replaceButtonsOnMenu(buildMenuButtons);
-}
+
