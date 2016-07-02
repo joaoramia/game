bd635403a165e0cda93974cfb1c6f6eba266fcf3
John Chen <john@johnchen.co>
2016-07-02 12:37:15 -0400

    Update unit.constructor.js

--- server/unit.constructor.js
+++ server/unit.constructor.js
@@ -4,6 +4,7 @@ function Unit (pos, type, maxHealth, attack, defense, speed, socketId) {
   this.selected = false;
   this.speed = speed;
   this.maxHealth = maxHealth; // integer
+  this.currentHealth = maxHealth;
   this.attack = attack; // integer
   this.defense = defense; // integer
   this.sprite = null; // should be set on front-end
@@ -12,16 +13,14 @@ function Unit (pos, type, maxHealth, attack, defense, speed, socketId) {
   this.socketId = socketId;
 }
 
-function Hero (socketId, pos, currentHealth){
+function Hero (socketId, pos){
   Unit.call(this, pos, 'hero', 100, 5, 1, 50, socketId);
   this.rateOfAttack = 1;
-  this.currentHealth = currentHealth || this.maxHealth;
 }
 
-function Soldier (socketId, pos, currentHealth){
+function Soldier (socketId, pos){
   Unit.call(this, pos, 'soldier', 60, 15, 0, 75, socketId);
   this.rateOfAttack = 5;
-  this.currentHealth = currentHealth || this.maxHealth;
 }
 
 module.exports = {
