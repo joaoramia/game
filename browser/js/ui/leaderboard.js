acd444697248323a99067672d1612af79eef5b70
joaoramia <joao.ramia@gmail.com>
2016-07-06 00:07:22 -0400

    almost done with leaderboard

--- browser/js/ui/leaderboard.js
+++ browser/js/ui/leaderboard.js
@@ -3,31 +3,29 @@ socket.on("leaderboardUpdate", function(playersData){
     for (var id in playersData) {
 		leaders.push([id, playersData[id].wealth, playersData[id].username]);
 	}
-	leaders.sort(function(a, b) { return a[1] - b[1]; });
+	leaders.sort(function(a, b) { return  b[1] - a[1]; });
 
     if (playersData){
         player.wealth = playersData[player.id].wealth;
         if (Object.keys(otherPlayers)){
-        	console.log("OTHER PLAYERS: ", otherPlayers);
             for (var id in otherPlayers){
                 if (otherPlayers[id] && otherPlayers[id].wealth) otherPlayers[id].wealth = playersData[id].wealth;
             }
         }
         if(player.id === currentKing){
-        // 	$("#kingname").text(player.username);
-        //     $("#king-wealth-display").text(commaSeparator(player.wealth));
-        // }
-        // else {
-        //     $("#king-wealth-display").text(commaSeparator(otherPlayers[currentKing].wealth));
+        	$("#kingname").text(player.username);
+            $("#king-wealth-display").text(commaSeparator(player.wealth));
+        }
+        else {
+        	$("#kingname").text(otherPlayers[currentKing].username);
+            $("#king-wealth-display").text(commaSeparator(otherPlayers[currentKing].wealth));
         }
     }
+    
+    console.log(leaders);
 
-    for (var i = 0; i < 5; i++){
-    	if (leaders[i] && i === 0) {
-    		$("#king-wealth-display").text(commaSeparator(leaders[i][1]));
-    		$("#kingname").text(leaders[i][2]);
-    	}
-    	if (leaders[i] && i !== 0) {
+    for (var i = 1; i < leaders.length; i++){
+    	if (leaders[i] && i > 0) {
     		$("#place" + (i + 1)).text(leaders[i][2] + " " + commaSeparator(leaders[i][1]));
     	}
     }
@@ -39,10 +37,6 @@ function commaSeparator(x) {
 }
 
 function sortPlayersPositions(obj){
-	// var maxSpeed = {
- //    car:300, bike:60, motorbike:200, airplane:1000,
- //    helicopter:400, rocket:8*60*60
-	// }
 	var sortable = [];
 	for (var id in obj) {
 		sortable.push([id, obj[id].wealth]);
