46796b309e1027a4f8a5466876617655bbd2cee6
Ani <aganita21@gmail.com>
2016-07-07 12:20:28 -0400

    enhanced the functionality of a chat. now user can hide the chat

--- browser/js/app.js
+++ browser/js/app.js
@@ -86,6 +86,7 @@ $('form').submit(function(){
 socket.on('chat message', function(msgObj){
     //$('#messages').append($('<li>').text(msgObj.username + " says "+ msgObj.text));
     $('#messages').append('<li>' + '<span style="color: '+ msgObj.msgcolor +'">' + msgObj.username + '</span> says "' + msgObj.text +'" </li>');
+    $('#chat-client').removeClass('display-none');
     $('#chat-client .message-panel')[0].scrollTop = 10000;
 });
 
