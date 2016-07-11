1342d02cfa964b079ab98decbe3401d2dccf3a26
Ani <aganita21@gmail.com>
2016-07-11 17:38:28 -0400

    removed unused count property on money bag object

--- index.js
+++ index.js
@@ -316,7 +316,6 @@ io.on('connection', function (socket) {
 
 //for generating money bags for the moneyBags object on server start
 function generateMoneyBags(count){
-    moneyBags.count = Object.keys(moneyBags).length - 1 + count;
     if (count === 1) {
         var keyName = [utils.getRandomNum(CANVAS_SIZE[0]), utils.getRandomNum(CANVAS_SIZE[1])]
         moneyBags[keyName] = {value : utils.getRandomNum(25, 75)};
