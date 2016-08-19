f9d49232cc121139574b0f6bc0085a68618d84c7
Joao Paulo Jung Ramia <joao.ramia@gmail.com>
2016-08-19 14:19:03 -0300

    Update ip.js

--- browser/js/ip.js
+++ browser/js/ip.js
@@ -1,1 +1,1 @@
-var ip = "54.173.229.200"; //ip of Heroku app (security issues here if game becomes popular)
+var ip = "54.175.230.252"; //ip of Heroku app (security issues here if game becomes popular)
